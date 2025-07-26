74a57c1f2e3988d8b64c2883af3188007db1b9fc
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2025-07-26 20:11:29 -0300

    feat: implement note fetching and display in dashboard
    - Added `getNotes` function to fetch user notes from Firestore.
    - Integrated note fetching in the Dashboard component using `useEffect`.
    - Display notes in a grid format with loading skeletons while fetching.
    - Added a message and button for creating notes when no notes are available.
    - Updated Firestore types to include `Note` structure.
    - Included a new SVG asset for note illustration.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogClose,
@@ -18,17 +18,21 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import {
   Card,
-  CardHeader,
   CardTitle,
   CardDescription,
   CardFooter,
+  CardHeader,
 } from "@/components/ui/card";
 import { useProtectedRoute } from "@/hooks/useProtectedRoute";
-import { saveNote } from "@/firebase/firestore";
+import { getNotes, Note, saveNote } from "@/firebase/firestore";
 import { toast } from "sonner";
 import { Loader2Icon } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 import { Toaster } from "@/components/ui/sonner";
+import { Skeleton } from "@/components/ui/skeleton";
+import { GoPlus } from "react-icons/go";
+import Image from "next/image";
+import NoteImg from "@/assets/note.svg";
 
 export default function Dashboard() {
   const { user, loading } = useProtectedRoute();
@@ -36,12 +40,11 @@ export default function Dashboard() {
   const [noteName, setNoteName] = useState("");
   const [noteDescription, setNoteDescription] = useState("");
   const [loadingCreategNote, setLoadingCreategNote] = useState(false);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loadingNotes, setLoadingNotes] = useState(true);
 
   const [openDialogSettings, setOpenDialogSettings] = useState(false);
   const [openDialogAddNote, setOpenDialogAddNotes] = useState(false);
-
-  if (loading) return <p>Carregando...</p>;
-  if (!user) return null;
 
   async function handleCreateNote(e: React.FormEvent) {
     e.preventDefault();
@@ -55,6 +58,10 @@ export default function Dashboard() {
       setNoteName("");
       setNoteDescription("");
       setOpenDialogAddNotes(false); // fecha o dialog
+
+      // atualiza as notas após criar uma nova
+      const updatedNotes = await getNotes(user.uid);
+      setNotes(updatedNotes);
     } catch (error) {
       console.error("Erro ao salvar nota:", error);
       toast.error("Erro ao salvar nota.");
@@ -62,6 +69,29 @@ export default function Dashboard() {
       setLoadingCreategNote(false);
     }
   }
+
+  useEffect(() => {
+    async function fetchNotes() {
+      if (!user) return;
+
+      try {
+        // chama a função que busca as notas do Firestore, passando o UID do usuário
+        const userNotes = await getNotes(user.uid);
+
+        // atualiza o estado com as notas encontradas
+        setNotes(userNotes);
+      } catch (error) {
+        console.error("Erro ao carregar notas:", error);
+      } finally {
+        setLoadingNotes(false);
+      }
+    }
+
+    fetchNotes();
+  }, [user]);
+
+  if (loading) return <p>Carregando...</p>;
+  if (!user) return null;
 
   return (
     <div>
@@ -71,15 +101,60 @@ export default function Dashboard() {
       />
       <div className="mx-auto my-12 max-w-6xl px-5">
         <div className="grid auto-rows-[250px] grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-          <Card className="flex flex-col rounded-md outline-none">
-            <CardHeader>
-              <CardTitle>Treino upper/lower</CardTitle>
-              <CardDescription>Nesse treino, vamos focar em...</CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <CardDescription>16/07/2025</CardDescription>
-            </CardFooter>
-          </Card>
+          {loadingNotes ? (
+            <>
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </>
+          ) : (
+            <>
+              {notes.map((note) => (
+                <Card
+                  key={note.id}
+                  className="flex flex-col justify-between rounded-md outline-none"
+                >
+                  <CardHeader>
+                    <CardTitle>{note.name}</CardTitle>
+                    <CardDescription>{note.description}</CardDescription>
+                  </CardHeader>
+                  <CardFooter className="mt-4">
+                    <CardDescription>
+                      {note.createdAt?.toDate
+                        ? new Date(note.createdAt.toDate()).toLocaleDateString()
+                        : "Data desconhecida"}
+                    </CardDescription>
+                  </CardFooter>
+                </Card>
+              ))}
+              {notes.length === 0 && (
+                <div className="col-span-full flex min-h-[60vh] w-full flex-col items-center justify-center">
+                  <Image
+                    alt="notes images"
+                    width={120}
+                    height={120}
+                    src={NoteImg}
+                    quality={100}
+                    className="size-36 md:size-48"
+                  />
+                  <div className="mt-4 flex flex-col items-center gap-2">
+                    <h1 className="text-muted-foreground mx-auto mb-2.5 max-w-xs text-center text-sm leading-tight">
+                      Você ainda não tem notas criadas. Clique no botão
+                      &quot;Criar Nota&quot; para começar!
+                    </h1>
+                    <Button
+                      variant="outline"
+                      onClick={() => setOpenDialogAddNotes(true)}
+                    >
+                      <GoPlus className="size-6" />
+                      Criar Nota
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </>
+          )}
         </div>
       </div>
       <Dialog open={openDialogAddNote} onOpenChange={setOpenDialogAddNotes}>
--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -4,9 +4,20 @@ import {
   addDoc,
   collection,
   doc,
+  DocumentData,
+  getDocs,
   serverTimestamp,
   setDoc,
 } from "firebase/firestore";
+
+export type Note = {
+  id: string;
+  name: string;
+  description: string;
+  createdAt?: {
+    toDate: () => Date;
+  };
+};
 
 export async function saveUserToFirestore(
   uid: string,
@@ -38,6 +49,35 @@ export async function saveNote(uid: string, name: string, description: string) {
     await addDoc(collection(db, "users", uid, "notes"), note);
   } catch (error) {
     toast.error("Erro ao salvar nota.");
+    throw error;
+  }
+}
+
+// recebe o `uid` do usuário e retorna uma Promise com um array de notas
+export async function getNotes(uid: string): Promise<Note[]> {
+  try {
+    // cria uma referência para a subcoleção "notes" dentro do documento do usuário no firestore
+    const notesRef = collection(db, "users", uid, "notes");
+    // faz a leitura (get) de todos os documentos dentro dessa subcoleção
+    const snapshot = await getDocs(notesRef);
+
+    // mapeia cada documento do snapshot para o formato `Note`
+    const notes: Note[] = snapshot.docs.map((doc: DocumentData) => {
+      // extrai os dados do documento (name, description, etc.)
+      const data = doc.data();
+
+      // retorna um objeto no formato `Note`
+      return {
+        id: doc.id,
+        name: data.name,
+        description: data.description,
+        createdAt: data.createdAt,
+      };
+    });
+
+    return notes;
+  } catch (error) {
+    toast.error("Erro ao buscar notas.");
     throw error;
   }
 }
